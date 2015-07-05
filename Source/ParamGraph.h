8a349e2465a576d95557b7c269b306efe4146612
Sauraen <sauraen@users.noreply.github.com>
2015-07-05 17:11:09 -0400

    Added copyright notices to source files

--- Source/ParamGraph.h
+++ Source/ParamGraph.h
@@ -1,12 +1,21 @@
-/*
-  ==============================================================================
+/*******************************************************************************
+timelagfilter: Open-source VST3 (x86/x64) plugin that uses phase effects to 
+    mangle audio in the time domain
+Copyright (C) 2015 Sauraen, sauraen@gmail.com
 
-    ParamGraph.h
-    Created: 13 May 2015 3:11:42pm
-    Author:  Sauraen
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-  ==============================================================================
-*/
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
 
 #ifndef PARAMGRAPH_H_INCLUDED
 #define PARAMGRAPH_H_INCLUDED
